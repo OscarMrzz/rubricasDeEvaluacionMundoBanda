3fee829ea22dd5919c7712da1f4bdaf483020fb3
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-09-06 00:18:18 -0600

    Add Poppins font to RootLayout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,13 @@ import FooterComponent from "@/component/FooterComponent/Page";
 import { Provider } from "react-redux";
 import store from "@/app/store"; // 👈 ajusta la ruta según tu proyecto
 
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 
 export default function RootLayout({
   children,
@@ -18,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="bg-[#1d3557] text-gray-50 grid grid-rows-[75px_1fr_100px] min-h-screen"
+        className={`bg-[#1d3557] text-gray-50 grid grid-rows-[75px_1fr_100px] min-h-screen font-poppins ${poppins.className} `}
       >
         <Provider store={store}>
           <header className="flex flex-col sticky top-0 z-50">
