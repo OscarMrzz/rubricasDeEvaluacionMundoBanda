a74df070307b7d945cae66b78ff55930c5054e50
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-10-13 15:38:50 -0600

    Refactor Page component layout for improved structure and user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,17 @@ const Page = () => {
 
 
   return (
-    <div className="w-full flex flex-col gap-4 p-6">
+    <div className="w-full h-screen p-10 justify-center  flex">
+      <div className="flex flex-col gap-4 w-full lg:w-lg ">
+
+     
       <h2 className="text-2xl font-black">Binevenido</h2>
       <p>Esta es la version Beta de la plataforma para jurados</p>
       <p>Gracias por ser parte de este proyecto</p>
       <p>Si esta biendo esto es por que confiamos en su opinion asi que agradeceremos nos haga saber sus impresiones, ideas y sugerencias</p>
               
       </div>
+       </div>
   );
      
 };
