3dd6cfd0e5c7a9ea196a34933f50cae78f0062d0
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-10-14 12:46:07 -0600

    Refactor PanelControlPage layout for improved structure and visual hierarchy

--- src/app/PanelControlPage/page.tsx
+++ src/app/PanelControlPage/page.tsx
@@ -1,58 +1,63 @@
 
+import { AdjustmentsHorizontalIcon, CalendarDaysIcon, ChartBarSquareIcon, ClipboardDocumentCheckIcon, DocumentTextIcon, ListBulletIcon, MapPinIcon, MusicalNoteIcon, UserIcon, UsersIcon } from '@heroicons/react/16/solid'
 import React from 'react'
 
 
 
 const PanelControlPage = () => {
   return (
-    <div className=' w-full h-screen   p-10 flex flex-col lg:flex-row  justify-center gap-6'>
-      <div className='flex flex-col gap-4 bg-gray-600  p-4 shadow-lg rounded-lg text-white'>
-
-
-      <h2 className='text-2xl font-black'>Panel de Administracion</h2>
-      <p >Desde aqui tine control total de la app</p>
-    <div className='flex flex-col gap-2'>
-
-  
-      <p className=' font-medium'>Acciones que puederealizar:    </p>
-      <ul className='font-light text-gray-100/80'>
-        <li className='font-thin'>Crear/Editar/Eliminar usuarios</li>
-        <li>Crear/Editar/Eliminar regiones</li>
-        <li>Crear/Editar/Eliminar eventos</li>
-        <li>Crear/Editar/Eliminar bandas</li>
-        <li>Crear/Editar/Eliminar Equipo Evaluador </li>
-        <li>Ver resultados de cada evento</li>
-        <li>ver los resultados de cada banda</li>
-        <li>Evaluar a las bandas</li>
-      
-      </ul>
-        </div>
-              </div>
-      <div className='flex flex-col gap-4 bg-gray-600  p-4 shadow-lg rounded-lg text-white'>
-
-
-      <h2 className='text-2xl font-black'>Recomendacion de uso</h2>
-
-    <div className='flex flex-col gap-2'>
-
-  
-  
-      <ul className='font-light text-gray-100/80'>
+    <div className='h-screen grid grid-cols-3 grid-rows-5 p-2 gap-2 '>
      
-        <li>Cree una region</li>
-        <li>Cree un evento (todo evento necesita una region)</li>
-            <li>Agregue un miembro al equipo evaluador del evento que creo (de doble click a la fila del evento) </li>
-        <li>Cree una categoria (coloquele el nombre que guste)</li>
-        <li>Agregue una banda (toda banda necesita una categoria)</li>
-        <li>cree una rubrica (toda rubrica necesita un categoria) </li>
-        <li>agregue criterio de evaluacion ( se agrega en la misma rubrica solo de doble click en la fila de la rubria)</li>
-        <li>agregue cumplimiento (segagrega dendotro del criterio solo de doble click a la fila del criterio)</li>
-    
-      
-      </ul>
-        </div>
-              </div>
-    
+    <div className='bg-gray-700 col-span-3 flex flex-col justify-center items-center text-2xl font-bold'>
+      <ClipboardDocumentCheckIcon className='w-12' />
+      <h2>Evaluar</h2>
+
+    </div>
+    <div className='bg-gray-700 flex flex-col justify-center items-center  font-bold'>
+      <MapPinIcon className='w-12' />
+      <h2>Region</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-1  flex flex-col justify-center items-center  font-bold'> 
+      <ListBulletIcon className='w-12' />
+      <h2 >Categorias</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold'>
+      <CalendarDaysIcon className='w-12' />
+      <h2>Eventos</h2>
+
+    </div >
+    <div className='bg-gray-700 col-span-2 flex flex-col justify-center items-center  font-bold'>
+      <MusicalNoteIcon className='w-12' />
+      <h2>Bandas</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-1 row-span-2 flex flex-col justify-center items-center  font-bold'>
+      <AdjustmentsHorizontalIcon className='w-12' />
+      <h2>Rubricas</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold'>
+      <ChartBarSquareIcon className='w-12' />
+      <h2 className='flex justify-center items-center text-center'>Resultados Evento</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold p-4'>
+      <DocumentTextIcon className='w-12' />
+      <h2 className='flex justify-center  text-center' >Reporte bandas</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-2 flex flex-col justify-center items-center  font-bold'>
+      <UsersIcon className='w-12' />
+      <h2>usuarios</h2>
+
+    </div>
+    <div className='bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold'>
+      <UserIcon className='w-12' />
+      <h2>Mi perfil</h2>
+
+    </div>
       
     </div>
   )
