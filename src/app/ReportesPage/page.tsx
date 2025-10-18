b92467fc1af3a356b74175b0df4a2feebcc5ca95
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-10-18 14:27:56 -0600

    Add category selection functionality and update data fetching logic in ResultadosGeneralesHomePage

--- src/app/ReportesPage/page.tsx
+++ src/app/ReportesPage/page.tsx
@@ -3,7 +3,7 @@
 import { use, useEffect, useRef, useState } from "react";
 import SkeletonTabla from "@/component/skeleton/SkeletonTabla/Page";
 import React from "react";
-import { RegistroEventoInterface, resultadosGeneralesInterface } from "@/interfaces/interfaces";
+import { categoriaInterface, RegistroEventoInterface, resultadosGeneralesInterface } from "@/interfaces/interfaces";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/app/store";
 import RegistroCumplimientoServices from "@/lib/services/RegistroCumplimientosServices";
@@ -11,18 +11,27 @@ import TablaResultadosGeneralesComponent from "@/component/Tablas/tablaResultado
 import ModalInformacionResultados from "@/component/informacion/informacionResultados/ModalInformacionResultados";
 import { desactivarOverleyInformacionResultados } from "@/feacture/resultadosGenerales/overlayResultados";
 import { useEventosStore } from "@/Store/EventosStore/listEventosStore";
+import { useCategoriasStore } from "@/Store/CategoriasStore/listCategoriaStore";
 export default function ResultadosGeneralesHomePage() {
   const registroCumpliminetoServices = useRef(new RegistroCumplimientoServices());
 
   const [resultados, setResultados] = useState<resultadosGeneralesInterface[]>([]);
 
   const activadorModalIformacionResultados = useSelector((state: RootState) => state.overletResultados);
+  const [categoriasSeleccionadas, setCategoriasSeleccionadas] = useState<categoriaInterface>();
+  const { listCategoriasStore } = useCategoriasStore();
 
   const dispatch = useDispatch();
 
   const { listEventosStore } = useEventosStore();
 
   
+  const [cargandoEventos, setCargandoEventos] = useState(false);
+  const [cargandoCategorias, setCargandoCategorias] = useState(false);
+  const [cargandoDatosTabla, setCargandoDatosTabla] = useState(false);
+  const [eventoSeleccionado, setEventoSeleccionado] = useState<RegistroEventoInterface>();
+  const [eventosLista, setEventosLista] = useState<RegistroEventoInterface[]>([]);
+  const [categoriasLista, setCategoriasLista] = useState<categoriaInterface[]>();
 
   useEffect(() => {
     if (listEventosStore.length > 0) {
@@ -31,13 +40,17 @@ export default function ResultadosGeneralesHomePage() {
       setEventosLista(listEventosStore);
       setCargandoEventos(false);
     }
+  
   }, [listEventosStore]);
 
-  const [cargandoEventos, setCargandoEventos] = useState(false);
-  const [cargandoDatosTabla, setCargandoDatosTabla] = useState(false);
+  useEffect(() => {
+    if (listCategoriasStore.length > 0) {
+      setCargandoCategorias(true);
+      setCategoriasLista(listCategoriasStore);
+      setCargandoCategorias(false);
+    }
+  }, [listCategoriasStore]);
 
-  const [eventoSeleccionado, setEventoSeleccionado] = useState<RegistroEventoInterface>();
-  const [eventosLista, setEventosLista] = useState<RegistroEventoInterface[]>([]);
 
 
 
@@ -47,24 +60,31 @@ export default function ResultadosGeneralesHomePage() {
 
    setEventoSeleccionado(JSON.parse(eventoLocalStorage));
  
-    }else{
-      traerDatosTabla("");
-
-
     }
 
 
   }, []);
 
+  useEffect(() => {
+    const categoriaLocalStorage = localStorage.getItem("CategoriaSelecionada");
+    if (categoriaLocalStorage && categoriaLocalStorage !== "undefined") {
+      setCategoriasSeleccionadas(JSON.parse(categoriaLocalStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (eventoSeleccionado && categoriasSeleccionadas) {
+      traerDatosTabla(eventoSeleccionado.idEvento, categoriasSeleccionadas.idCategoria);
+    }
+  }, []);
 
 
-
-  async function traerDatosTabla(idEvento: string) {
-    if (idEvento !== "") {
+  async function traerDatosTabla(idEvento: string, idCategoria: string) {
+    if (idEvento !== "" && idEvento !== "") {
    
       try {
         const resultadosData: resultadosGeneralesInterface[] =
-          await registroCumpliminetoServices.current.getResultadosEvento(idEvento);
+          await registroCumpliminetoServices.current.getResultadosEventoYCategoria(idEvento, idCategoria);
 
         setResultados(resultadosData);
     
@@ -77,11 +97,13 @@ export default function ResultadosGeneralesHomePage() {
   }
   useEffect(() => {
     const eventoLocalStorage = localStorage.getItem("EventoSelecionado");
+    const categoriaLocalStorage = localStorage.getItem("CategoriaSelecionada");
 
-    if (eventoLocalStorage && eventoLocalStorage !== "undefined") {
+    if (eventoLocalStorage && eventoLocalStorage !== "undefined" &&
+        categoriaLocalStorage && categoriaLocalStorage !== "undefined") {
 
       setEventoSeleccionado(JSON.parse(eventoLocalStorage)  );
-      traerDatosTabla(JSON.parse(eventoLocalStorage).idEvento );
+      traerDatosTabla(JSON.parse(eventoLocalStorage).idEvento, JSON.parse(categoriaLocalStorage).idCategoria) 
     }
 
 
@@ -93,8 +115,15 @@ export default function ResultadosGeneralesHomePage() {
     setEventoSeleccionado(evento);
     localStorage.setItem("EventoSelecionado", JSON.stringify(evento));
     setResultados([]);
-    traerDatosTabla(idEvento);
+    traerDatosTabla(idEvento, categoriasSeleccionadas ? categoriasSeleccionadas.idCategoria : "");
   };
+  const selecionarCategoria = (idCategoria: string) => {
+    const categoria = categoriasLista?.find((categoria) => categoria.idCategoria === idCategoria);
+    setCategoriasSeleccionadas(categoria);
+    localStorage.setItem("CategoriaSelecionada", JSON.stringify(categoria));
+    setResultados([]);
+    traerDatosTabla(eventoSeleccionado ? eventoSeleccionado.idEvento : "", idCategoria);
+  }
 
   return (
     <>
@@ -110,7 +139,7 @@ export default function ResultadosGeneralesHomePage() {
             <div className="flex justify-between items-center mb-4">
               <h1 className="text-2xl font-bold mb-4">Eventos</h1>
             </div>
-            <div className="flex justify-between mb-4">
+            <div className="flex  gap-4 pb-2">
               <div className="flex gap-4">
                 <select
                   className=" w-40 h-6 bg-red-500 border-0"
@@ -133,6 +162,34 @@ export default function ResultadosGeneralesHomePage() {
                       {eventosLista.map((evento) => (
                         <option className="bg-white text-gray-800" key={evento.idEvento} value={evento.idEvento}>
                           {evento.LugarEvento}
+                        </option>
+                      ))}
+                    </>
+                  )}
+                </select>
+              </div>
+              <div className="flex gap-4">
+                <select
+                  className=" w-40 h-6 bg-red-500 border-0"
+                  name=""
+                  id=""
+                  value={categoriasSeleccionadas?.idCategoria}
+                  onChange={(event) => {
+                    selecionarCategoria(event.target.value);
+                  }}
+                >
+                  {cargandoEventos ? (
+                    <option className="bg-white text-gray-400" value="">
+                      Categorias...
+                    </option>
+                  ) : (
+                    <>
+                      <option className="bg-white text-gray-400" value="">
+                        Categorias
+                      </option>
+                      {categoriasLista?.map((categoria) => (
+                        <option className="bg-white text-gray-800" key={categoria.idCategoria} value={categoria.idCategoria}>
+                          {categoria.nombreCategoria}
                         </option>
                       ))}
                     </>
