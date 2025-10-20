c46ae9970b495ef724d9042c05265dcd5c17d6a0
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-10-19 22:10:05 -0600

    Refactor BandasHomePage component to use refs for services and enhance modal functionality

--- src/app/PanelControlPage/bandasHomePage/page.tsx
+++ src/app/PanelControlPage/bandasHomePage/page.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BandasServices from "@/lib/services/bandasServices";
 import { bandaDatosAmpleosInterface, categoriaInterface} from "@/interfaces/interfaces";
 import SkeletonTabla from "@/component/skeleton/SkeletonTabla/Page";
 import React from "react";
 
-import TablaBandasComponent from "@/component/Tablas/tablaBandasComponet/Page";
 import OverleyModalFormulario from "@/component/modales/OverleyModalFormulario/Page";
 import FormularioAgregarBandaComonent from "@/component/formularios/bandaFormulario/formularioAgregarBandaComponent/Page";
 import { PlusIcon } from "@heroicons/react/16/solid";
 import CategoriasServices from "@/lib/services/categoriaServices";
+import OverleyModal from "@/component/modales/OverleyModal/Page";
+import InformacionBandaComponent from "@/component/informacion/informacionBandaComponent/Page";
+import FormularioEditarBandaComponent from "@/component/formularios/bandaFormulario/formularioEditarBandaComponent/Page";
+
 
 const BandasHomePage = () => {
-  const [bandas, setBandas] = useState<bandaDatosAmpleosInterface[]>([]);
 
-  const [categorias, setCategorias] = useState<categoriaInterface[]>([]);
+  const bandasServices = useRef(new BandasServices())
+  const categoriasServices = useRef(new CategoriasServices())
+
+  const [bandasList, setBandasList] = useState<bandaDatosAmpleosInterface[]>([]);
+
+  const [categoriasList, setCategoriasList] = useState<categoriaInterface[]>([]);
+    const [selectedBanda, setSelectedBanda] =React.useState<bandaDatosAmpleosInterface | null>(null);
+      const [openFormEditar, setOpenFormEditar] = React.useState(false);
  
   const [loading, setLoading] = useState(true);
   const [formularioAgregarHabierto, setFormularioAgregarHabierto] = useState(false);
+
+   const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     traerDatosTabla();
@@ -26,14 +37,13 @@ const BandasHomePage = () => {
 
 
   async function traerDatosTabla() {
-    const bandasServices = new BandasServices();
-    const categoriasServices = new CategoriasServices();
+
     try {
-      const BandasData: bandaDatosAmpleosInterface[] =await bandasServices.getDatosAmpleos();
-      const categoriasData: categoriaInterface[] = await categoriasServices.get();
+      const BandasData: bandaDatosAmpleosInterface[] =await bandasServices.current.getDatosAmpleos();
+      const categoriasData: categoriaInterface[] = await categoriasServices.current.get();
       
-      setBandas(BandasData);
-      setCategorias(categoriasData);
+      setBandasList(BandasData);
+      setCategoriasList(categoriasData);
       setLoading(false);
      
     } catch (error) {
@@ -45,40 +55,95 @@ const BandasHomePage = () => {
     setFormularioAgregarHabierto(true);
   };
 
+    const cerrarModal = () => {
+    setOpen(false);
+  };
 
-  
+    const cerrarModalEditar = () => {
+    setOpenFormEditar(false);
+  };
+
+    const abrirModalEditar = () => {
+    setOpenFormEditar(true);
+
+  };
+
+  const abrirModalInformacion = (banda: bandaDatosAmpleosInterface) => {
+    setSelectedBanda(banda);
+    setOpen(true);
+  };
 
   return (
     <>
    
-    <div className=" px-25 mx-20  pb-25"
+    <div className=" w-full    pb-25"
    >
       <div className="flex justify-between items-center mb-4">
 
       <h1 className="text-2xl font-bold mb-4">Bandas</h1>
       <button className="cursor-pointer flex justify-center items-center gap-2"  onClick={abrirFormularioAgregar} >
-        <PlusIcon className="w-5 h-5 bg-blue-600  rounded-2xl" />
+        <PlusIcon className="w-5 h-5   rounded-2xl" />
         Agregar</button>
       </div>
-        <div className="">
+        <div className=" w-full ">
+
+            <OverleyModal open={open} onClose={cerrarModal}>
+          {selectedBanda && (
+            <InformacionBandaComponent
+              Banda={selectedBanda}
+              onClose={cerrarModal}
+              onRefresh={traerDatosTabla}
+              openFormEditar={abrirModalEditar}
+            /> 
+          )}
+        </OverleyModal>
 
       <OverleyModalFormulario   open={formularioAgregarHabierto} onClose={() => setFormularioAgregarHabierto(false)}>
         <FormularioAgregarBandaComonent refresacar={traerDatosTabla} onClose={() => setFormularioAgregarHabierto(false)} />
 
       </OverleyModalFormulario>
 
-      {loading ? (
-        <SkeletonTabla />
-      ) : (
-      
-
-       
-        <TablaBandasComponent
-          bandas={bandas}
-          categorias={categorias}
-          onRefresh={traerDatosTabla}
+          <OverleyModalFormulario 
+      open={openFormEditar} 
+      onClose={cerrarModalEditar} 
+    >
+      {selectedBanda && (
+        <FormularioEditarBandaComponent
+          onClose={cerrarModalEditar}
+          refresacar={traerDatosTabla}
+          bandaAEditar={selectedBanda}
         />
       )}
+    </OverleyModalFormulario>
+
+      {loading ? (
+        <SkeletonTabla />
+      ) : <>
+        <div className="flex flex-col gap-4  w-full">
+          {
+            bandasList.map((banda) => (
+              <div 
+              key={banda.idBanda}
+              className="w-full h-25 bg-gray-700 p-4 cursor-pointer hover:bg-gray-600"
+              onDoubleClick={() => abrirModalInformacion(banda)}
+              
+              >
+                <h2 className="text-xl font-bold">{banda.nombreBanda}</h2>
+                <p>Alias: {
+                 banda.AliasBanda
+                }</p>
+
+
+
+              </div>
+            ))
+          }
+        </div>
+      </>
+    }
+      
+       
+    
       </div>
     </div>
     </>
