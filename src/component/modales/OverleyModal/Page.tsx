7a5bac1279fccd831802a27aa428d44e7e800c38
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-10-20 21:40:16 -0600

    Refactor: Enhance layout structure and button placement in OverleyModal component

--- src/component/modales/OverleyModal/Page.tsx
+++ src/component/modales/OverleyModal/Page.tsx
@@ -25,10 +25,17 @@ const OverleyModal = ({open, onClose, children}:OverleyModalProps) => {
 <>
   {open ? (
     <div onDoubleClick={onClose}  className="bg-gray-950/50 inset-0 z-100 fixed w-screen h-screen flex justify-center items-center">
-      <div className={`w-2xl h-120 bg-[#415a77] ${Animar ? 'scale-100' : 'scale-75'}  transition-all duration-500 ease-in-out`}>
-      
+      <div className={`flex flex-col  w-2xl h-120 bg-[#415a77] ${Animar ? 'scale-100' : 'scale-75'}  transition-all duration-500 ease-in-out `}>
+      <div className='h-full'>
+
         {children}
       </div>
+    <div className=' flex justify-end p-4'>
+
+      <button onClick={onClose}  className='border-2 px-4 py-2 cursor-pointer hover:bg-gray-200/20'>Cerrar</button>
+    </div>
+      </div>
+    
     </div>
   ) : null}
 </>
