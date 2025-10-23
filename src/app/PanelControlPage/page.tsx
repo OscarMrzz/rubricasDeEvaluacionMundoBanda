0591f721c6839396400f2f6b0de4449329607457
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-10-23 01:03:31 -0600

    Refactor: Update background color classes from gray to slate in PanelControlPage links

--- src/app/PanelControlPage/page.tsx
+++ src/app/PanelControlPage/page.tsx
@@ -18,7 +18,7 @@ const PanelControlPage = () => {
     <div className="h-screen grid grid-cols-3 grid-rows-5 p-2 gap-2 ">
       <Link
         href="/EvaluarPage"
-        className="bg-gray-700 col-span-3 flex flex-col justify-center items-center text-2xl font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-3 flex flex-col justify-center items-center text-2xl font-bold hover:bg-slate-600 cursor-pointer"
       >
         <ClipboardDocumentCheckIcon className="w-12" />
         <h2>Evaluar</h2>
@@ -26,63 +26,63 @@ const PanelControlPage = () => {
 
       <Link
         href="/PanelControlPage/regionHomePage"
-        className="bg-gray-700 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <MapPinIcon className="w-12" />
         <h2>Region</h2>
       </Link>
       <Link
         href="/PanelControlPage/categoriasHomePage"
-        className="bg-gray-700 col-span-1  flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-1  flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <ListBulletIcon className="w-12" />
         <h2>Categorias</h2>
       </Link>
       <Link
         href="/PanelControlPage/eventosHomePage"
-        className="bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <CalendarDaysIcon className="w-12" />
         <h2>Eventos</h2>
       </Link>
       <Link
         href="/PanelControlPage/bandasHomePage"
-        className="bg-gray-700 col-span-2 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-2 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <MusicalNoteIcon className="w-12" />
         <h2>Bandas</h2>
       </Link>
       <Link
         href="/PanelControlPage/rubricaHomePage"
-        className="bg-gray-700 col-span-1 row-span-2 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-1 row-span-2 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <AdjustmentsHorizontalIcon className="w-12" />
         <h2>Rubricas</h2>
       </Link>
       <Link
         href="/ReportesPage"
-        className="bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <ChartBarSquareIcon className="w-12" />
         <h2 className="flex justify-center items-center text-center">Resultados Evento</h2>
       </Link>
       <Link
         href="/reportePorBandaPage"
-        className="bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer p-4"
+        className="bg-slate-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer p-4"
       >
         <DocumentTextIcon className="w-12" />
         <h2 className="flex justify-center  text-center">Reporte bandas</h2>
       </Link>
       <Link
         href="/PanelControlPage/usuariosHomePage"
-        className="bg-gray-700 col-span-2 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-2 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <UsersIcon className="w-12" />
         <h2>usuarios</h2>
       </Link>
       <Link
         href="/miPerfilPage"
-        className="bg-gray-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-gray-600 cursor-pointer"
+        className="bg-slate-700 col-span-1 flex flex-col justify-center items-center  font-bold hover:bg-slate-600 cursor-pointer"
       >
         <UserIcon className="w-12" />
         <h2>Mi perfil</h2>
