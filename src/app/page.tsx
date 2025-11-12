faee3a5109621bff2cd224bde01aba555322de41
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-11-12 17:20:29 -0600

    Fix: Correct typo in welcome message on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const Page = () => {
       <div className="flex flex-col gap-4 w-full lg:w-lg ">
 
      
-      <h2 className="text-2xl font-black">Binevenido</h2>
+      <h2 className="text-2xl font-black">Bienvenido.</h2>
       <p>Esta es la version Beta de la plataforma para jurados</p>
       <p>Gracias por ser parte de este proyecto</p>
       <p>Si esta biendo esto es por que confiamos en su opinion asi que agradeceremos nos haga saber sus impresiones, ideas y sugerencias</p>
